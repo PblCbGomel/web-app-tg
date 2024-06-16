bbc78efdbe2a0eac6a77e9ff9dcda5ef1a6cea86
PblCbGomel <pblcb.gomel@gmail.com>
2024-06-17 00:42:15 +0300

    changes

--- src/pages/main-page.jsx
+++ src/pages/main-page.jsx
@@ -1,11 +1,19 @@
 import { Character } from "../components/character/character";
 import { MainHeader } from "../components/main-header/main-header";
 
+function CharacterWrapper() {
+  return (
+    <>
+      <Character />
+    </>
+  );
+}
+
 export function MainPage() {
   return (
     <>
       <MainHeader />
-      <Character />
+      <CharacterWrapper />
     </>
   );
 }
