77421e49170f8dca10a34cb09fd77621a591e72f
PblCbGomel <pblcb.gomel@gmail.com>
2024-06-18 14:52:28 +0300

    visual fixes

--- src/components/main-header/main-header.jsx
+++ src/components/main-header/main-header.jsx
@@ -13,7 +13,7 @@ export function MainHeader() {
           src="./images/photo.webp"
           className="user-photo"
         />
-        {window.Telegram?.initDataUnsafe?.user?.username || "UserName"}
+        {tg.initData + tg?.initDataUnsafe?.user?.username || "UserName"}
       </div>
       <div className="streak">
         <div>000</div>
