9b9f0678c13ff45f8780877578a6a4583ce834b8
PblCbGomel <pblcb.gomel@gmail.com>
2024-06-26 01:04:13 +0300

    add functional of update data

--- src/components/character/character.jsx
+++ src/components/character/character.jsx
@@ -1,14 +1,32 @@
 import "./character.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { ScoreContext } from "../score-context/score-context";
 
 export function Character() {
   const { energy, coins, setEnergy, setCoins } = useContext(ScoreContext);
 
+  let energyToUpdate = 0;
+  let coinsToUpdate = 0;
+
+  function updateDataOnServer() {
+    //запрос для изменения количества энергии и монеток на energyToUpdate coinsToUpdate
+  }
+
+  const intervalUpdate = setInterval(updateDataOnServer, 1000);
+
   const handleOnclick = () => {
+    energyToUpdate++;
+    coinsToUpdate++;
     setEnergy(energy + 1);
     setCoins(coins + 1);
   };
+
+  useEffect(() => {
+    return () => {
+      updateDataOnServer();
+      clearInterval(intervalUpdate);
+    };
+  });
 
   return <div className="character" onClick={handleOnclick}></div>;
 }
